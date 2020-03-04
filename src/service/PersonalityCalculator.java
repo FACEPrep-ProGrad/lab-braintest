4a10d6c3d14855fb37226179b739e379f69765bf
souru98 <kulkarnisourabh1998@gmail.com>
2020-03-04 22:34:23 +0530

    my code

--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -9,30 +9,35 @@ package service;
 // Use the split method to segregate the options and store it in a string array.
 // Convert the string array into an integer array.
 // return the entire integer array to findyourBrainType
+
+//class personality calculator
 public class  PersonalityCalculator
 {
+	
+	//method find answer
 	public int[] findAnswers(String options)
 	{
 		String c[]=options.split(",");
-		 int n=c.length;
-		int a[]=new int[n];
+		int value=c.length;
+		int array[]=new int[value];
 		int i=0;
 		for(String s:c)
 		{
-			a[i]=Integer.parseInt(s);
+			array[i]=Integer.parseInt(s);
 			i++;
-			
 		}
 		
-		
-		return a;
+		return array;
 		
 	}
+	
+	
+	//check brain type
 	public String findYourBrainType(String options)
 	{ 
-		int a[]=findAnswers(options);
-		int x=a[0]+a[1]+a[2]+a[4]+a[7]+a[9]+a[10]+a[11]+a[13]+a[17]+a[19];
-		int y=a[3]+a[5]+a[6]+a[12]+a[14]+a[15]+a[16]+a[18];
+		int array[]=findAnswers(options);
+		int x=array[0]+array[1]+array[2]+array[4]+array[7]+array[9]+array[10]+array[11]+array[13]+array[17]+array[19];
+		int y=array[3]+array[5]+array[6]+array[12]+array[14]+array[15]+array[16]+array[18];
 		int z=66-x+y;
 		System.out.println("Yor total score "+z);
 		if((z>=22)&&(z<=55))
@@ -43,7 +48,6 @@ public class  PersonalityCalculator
              return "Right-brained";
 		return "false";		
 				
-		
 	}
 	
 }
