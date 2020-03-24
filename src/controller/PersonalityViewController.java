252a1e86c17c7d8c1a25fdee8fcd595f127dfb95
PRiNCE BHARDWAj <Prince>
2020-03-24 23:35:11 +0530

    FSJ0320A021

--- src/controller/PersonalityViewController.java
+++ src/controller/PersonalityViewController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import service.PersonalityCalculator;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -33,8 +34,6 @@ public class PersonalityViewController extends HttpServlet {
 		String options=request.getParameter("selectedOptions");
 		
 		System.out.println(options);
-				
-		/* 
 
 		request.setAttribute("message", message);
 		
@@ -53,8 +52,11 @@ public class PersonalityViewController extends HttpServlet {
 				rd.forward(request, response);
 			}
 		
-		}*/
 		}
+		}
+	public static void main(String[] args) {
+		PersonalityCalculator obj=new PersonalityCalculator();
+		String message = obj.findYourBrain(options);
 	}
-
-
+	
+	}
--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -1,2 +1,34 @@
 package service;
 
+public class PersonalityCalculator{
+	String[] options = {"5","2","3","1","4","3","2","1","5","4","3","2","1","5","4","3","2","1","2","2"};
+	int[] optionsInt=new int[options.length];
+	
+	//FUNCTION findAnswers
+	public int[] findAnswers(String options) {
+		for(int i=0;i<this.options.length;i++) {
+			optionsInt[i]=Integer.parseInt(this.options[i]);
+		}
+		return optionsInt;
+	}
+	
+	//FUNCTION findYourBrainType
+	public String findYourBrainType(String options) {
+		int A=0,B=0;
+		findAnswers(options);
+		for(int i=0;i<optionsInt.length;i++) {
+			A+=optionsInt[i];
+		}
+		int x=66-A+B;
+		
+		if(x>=20 && x<=55) {
+			return "leftbrained";
+		}
+		else if(x>=56 && x<=64) {
+			return "noclearpreference";
+		}
+		else {
+			return "rightbrained";
+		}
+	}
+}
