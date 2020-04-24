32fee7256715b188b0f52b3e1d27fddcfffa36e8
subhamdas226 <53463561+subhamdas226@users.noreply.github.com>
2020-04-24 11:27:23 +0530

    Update PersonalityCalculator.java

--- src/service/PersonalityCalculator.java
+++ src/service/PersonalityCalculator.java
@@ -8,10 +8,8 @@ public class PersonalityCalculator{
 				B = (Q4) +(Q6)+(Q7) + (Q9) + (Q13) + (Q15) + (Q16) +(Q17)+ (Q19)  = _____ 
 				            
 				X = 66 - _A_ + _B_ =*/
-		int a = 0;
-		for(int i=0;i<20;i++) {
-			a = a + arr[i];
-		}
+		
+		int a = arr[0] +arr[1] +arr[2] +arr[4] +arr[7] +arr[10] +arr[11] +arr[13] +arr[17] +arr[19];
 		int b = arr[3] +arr[5] +arr[6] +arr[8] +arr[12] +arr[14] +arr[15] +arr[16] +arr[18];
 		int x = 66 - a + b;
 		/*
